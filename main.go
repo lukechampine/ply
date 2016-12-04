d3f06def67bb7f121792e63fc3fc74a733807795
lukechampine <luke.champine@gmail.com>
2016-12-04 06:01:55 -0500

    first ply compiler
    Running main.go will compile test.ply to Go and print the result to
    stdout.
    
    The basic process is:
    - Parse and type-check test.ply (remember, we modified the type-
      checker so that it knows about 'merge').
    - Walk the AST, looking for ocurrences of 'merge'.
      - For each occurrence, lookup the argument types for that
        expression (this is why we need to do type-checking).
      - Replace the occurrence with its 'specialized' name, e.g.
        merge -> mergeintint.
      - Generate a specialized function declaration for the occurrence.
    - Add each new declaration to the ast.File.
    - Print the ast.File to stdout.
    
    I'm no expert when it comes to the go/ packages, so there's probably
    a cleaner/easier way to accomplish this. Regardless, it works, and
    it's not to hard to imagine even adding optimizations. For example,
    in the following code:
    
        m1 = merge(m1, m2)
    
    Our specialized 'merge' function could simply copy m2's elements
    into m1 (instead of allocating a new map, as it currently does). For
    the most part, though, I plan to leave such optimizations to the Go
    compiler. In a similar vein, it seems obvious that passing 'nil' in
    place of either map argument should be valid; this requires a slight
    tweak to the type-checker. But as I stated previously, it remains to
    be seen whether 'merge' will make the final cut anyway. For now, it
    is simply a proof-of-concept.

---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/printer"
+	"go/token"
+	"os"
+
+	"github.com/lukechampine/ply/types"
+)
+
+const mergeTempl = `package ply
+func merge%[1]s%[2]s(m1, m2 map[%[1]s]%[2]s) map[%[1]s]%[2]s {
+	m3 := make(map[%[1]s]%[2]s)
+	for k, v := range m1 {
+		m3[k] = v
+	}
+	for k, v := range m2 {
+		m3[k] = v
+	}
+	return m3
+}
+`
+
+// A specializer is an ast.Visitor that inserts specialized versions of each
+// generic ply function, and rewrites the callsites to use their corresponding
+// specialized function.
+type specializer struct {
+	types    map[ast.Expr]types.TypeAndValue
+	newDecls map[string]ast.Decl
+}
+
+func (s specializer) Visit(node ast.Node) ast.Visitor {
+	switch n := node.(type) {
+	case *ast.CallExpr:
+		fn, ok := n.Fun.(*ast.Ident)
+		if !ok {
+			// ply functions are always idents
+			return s
+		}
+		if fn.Name == "merge" {
+			mt := s.types[n.Args[0]].Type.(*types.Map)
+			fn.Name += mt.Key().String() + mt.Elem().String()
+			if _, ok := s.newDecls[fn.Name]; !ok {
+				// check for existence first, because constructing a new decl
+				// is expensive
+				fset := token.NewFileSet()
+				code := fmt.Sprintf(mergeTempl, mt.Key().String(), mt.Elem().String())
+				// TODO: is there an easier way than ParseFile?
+				f, err := parser.ParseFile(fset, "", code, 0)
+				if err != nil {
+					panic(err)
+				}
+				s.newDecls[fn.Name] = f.Decls[0]
+			}
+		}
+	}
+	return s
+}
+
+func main() {
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, "test.ply", nil, 0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	info := types.Info{
+		Types: make(map[ast.Expr]types.TypeAndValue),
+	}
+	var conf types.Config
+	_, err = conf.Check("ply", fset, []*ast.File{f}, &info)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// modify the AST as needed
+	spec := specializer{
+		types:    info.Types,
+		newDecls: make(map[string]ast.Decl),
+	}
+	ast.Walk(spec, f)
+	for _, decl := range spec.newDecls {
+		f.Decls = append(f.Decls, decl)
+	}
+
+	// output modified AST
+	printer.Fprint(os.Stdout, fset, f)
+}
